2ba48642ba77a7cbf7bbbf30510af901e34ab771
Mateus <38964103+mateushvenancio@users.noreply.github.com>
2022-03-30 21:30:18 -0300

    navbar scaffold

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeModule } from './home/home.module';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, NavbarComponent],
   imports: [BrowserModule, AppRoutingModule, HomeModule],
   bootstrap: [AppComponent],
 })
